af6eda95d6f567a11e2a5d132cba5d240b3c8cae
vlad1p <vlad1p.game@gmail.com>
2022-12-06 20:02:28 +0300

    Add InputService

---
+++ Assets/CodeBase/Services/Input/InputService.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services.Input
+{
+    public class InputService : IInputService
+    {
+        private const string Horizontal = "Horizontal";
+        private const string Vertical = "Vertical";
+        private const string Button = "Fire";
+
+        public Vector2 Axis =>
+            new Vector2(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
+
+        public bool isAttackButton() => 
+            SimpleInput.GetButtonUp(Button);
+    }
+}
---
+++ Assets/CodeBase/Services/Input/MobileInputService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services.Input
+{
+    class MobileInputSerivece : InputService
+    {
+        public override Vector2 Axis => SimpleInputAxis();
+    }
+}
