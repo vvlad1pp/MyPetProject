5b965c4ac323c920a114fcf8fb50255cce1fe440
vlad1p <vlad1p.game@gmail.com>
2023-01-06 12:08:20 +0300

    add curtain loadScene

---
+++ Assets/CodeBase/Infrastructure/GameLoopState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeBase.Infrastructure
+{
+    public class GameLoopState : IState
+    {
+        public GameLoopState(GameStateMachine stateMachine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exit()
+        {
+        }
+
+        public void Enter()
+        {  
+        }
+    }
+}
---
+++ Assets/CodeBase/Logic/LoadingCurtain.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeBase.Logic
+{
+    public class LoadingCurtain
+    {
+        
+    }
+}
